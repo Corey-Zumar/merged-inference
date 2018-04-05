964951b4792def9e5870970fdb3ccabcbcf4e3df
Ankit Mathur <ankitmathur@berkeley.edu>
2018-04-05 16:09:11 -0700

    add tested combined-dense layer, implement combined bias/activation operations when possible, integrate dense merging option into mnist_dense, include model graph saving

--- layer_fusion.py
+++ layer_fusion.py
@@ -2,8 +2,9 @@ import tensorflow as tf
 
 # Code that attempts to find candidate layers to merge together for inference time
 # worthy experiment: How does cuBLAS matrix multiply performance scale with matrix size?
-# Hard constraints: GPU multiply unit size (12GB), multiplication precision
-# Soft constraints: For small models, we could probably run quite a few kernels
+# worhty experiment: is a convolution way more expensive than a matrix multiply?
+# Hard constraints: GPU multiply unit size (12GB - (1/3) * 12 = 8GB), multiplication precision
+# Soft constraints: For small models, we could probably merge quite a few kernels
 
 # TODO: Remove layers with unused output nodes
 
@@ -14,22 +15,20 @@ import tensorflow as tf
 # get colocated models
 g1 = tf.Graph()
 g2 = tf.Graph()
-export_dir1 = 'models/model1.ckpt'
+export_dir1 = 'tensorflow_models/models/model_serving'
 export_dir2 = 'models/model2.ckpt'
 with tf.Session(graph=g1) as sess1:
-	tf.saved_model.loader.load(sess, [tag_constants.TRAINING], export_dir1)
-	print(sess1.graph.get_operations())
+    tf.saved_model.loader.load(sess1, [tf.saved_model.tag_constants.SERVING], export_dir1)
+    print(sess1.graph.get_operations())
 
-with tf.Session(graph=g2) as sess:
-  tf.saved_model.loader.load(sess, [tag_constants.TRAINING], export_dir2)
-  print(sess2.graph.get_operations())
+# with tf.Session(graph=g2) as sess:
+#     tf.saved_model.loader.load(sess, [tag_constants.TRAINING], export_dir2)
+#     print(sess2.graph.get_operations())
 
 global_unique_tensors = {'input'}
 
 # cost estimates of each layer will make it easier to figure out which computations are easiest to merge.
-
-
-
+# compute average or hardcode
 # iterate through model graphs and categorize them
 
 # iterate through categorized layers and evaluate them for combination
--- tensorflow_models/merged_dense.py
+++ tensorflow_models/merged_dense.py
@@ -2,199 +2,139 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from tensorflow.python.eager import context
+import tensorflow as tf
 from tensorflow.python.framework import ops
 from tensorflow.python.framework import tensor_shape
 from tensorflow.python.layers import base
-from tensorflow.python.layers import utils
-from tensorflow.python.ops import array_ops
-from tensorflow.python.ops import init_ops
-from tensorflow.python.ops import math_ops
 from tensorflow.python.ops import nn
 from tensorflow.python.ops import standard_ops
 
+
 class CombinedDenseSameInput(base.Layer):
-  """Combination layer that combines tf.Dense operators for two different graphs Densely-connected layer class.
+    """Combination layer that combines tf.Dense operators for two different graphs Densely-connected layer class.
 
-  This layer implements the operation:
-  `outputs = activation(inputs * kernel + bias)`
-  Where `activation` is the activation function passed as the `activation`
-  argument (if not `None`), `kernel` is a weights matrix created by the layer,
-  and `bias` is a bias vector created by the layer
-  (only if `use_bias` is `True`).
+    This layer implements the operation:
+    `outputs = activation(inputs * kernel + bias)`
+    Where `activation` is the activation function passed as the `activation`
+    argument (if not `None`), `kernel` is a weights matrix created by the layer,
+    and `bias` is a bias vector created by the layer
+    (only if `use_bias` is `True`).
 
-  Arguments:
-    units: Integer or Long, dimensionality of the output space.
-    activation: Activation function (callable). Set it to None to maintain a
-      linear activation.
-    use_bias: Boolean, whether the layer uses a bias.
-    kernel_initializer: Initializer function for the weight matrix.
-      If `None` (default), weights are initialized using the default
-      initializer used by `tf.get_variable`.
-    bias_initializer: Initializer function for the bias.
-    kernel_regularizer: Regularizer function for the weight matrix.
-    bias_regularizer: Regularizer function for the bias.
-    activity_regularizer: Regularizer function for the output.
-    kernel_constraint: An optional projection function to be applied to the
-        kernel after being updated by an `Optimizer` (e.g. used to implement
-        norm constraints or value constraints for layer weights). The function
-        must take as input the unprojected variable and must return the
-        projected variable (which must have the same shape). Constraints are
-        not safe to use when doing asynchronous distributed training.
-    bias_constraint: An optional projection function to be applied to the
-        bias after being updated by an `Optimizer`.
+    Arguments:
+    layers_to_combine
     trainable: Boolean, if `True` also add variables to the graph collection
       `GraphKeys.TRAINABLE_VARIABLES` (see `tf.Variable`).
     name: String, the name of the layer. Layers with the same name will
       share weights, but to avoid mistakes we require reuse=True in such cases.
-    reuse: Boolean, whether to reuse the weights of a previous layer
-      by the same name.
+    """
 
-  Properties:
-    units: Python integer, dimensionality of the output space.
-    activation: Activation function (callable).
-    use_bias: Boolean, whether the layer uses a bias.
-    kernel_initializer: Initializer instance (or name) for the kernel matrix.
-    bias_initializer: Initializer instance (or name) for the bias.
-    kernel_regularizer: Regularizer instance for the kernel matrix (callable)
-    bias_regularizer: Regularizer instance for the bias (callable).
-    activity_regularizer: Regularizer instance for the output (callable)
-    kernel_constraint: Constraint function for the kernel matrix.
-    bias_constraint: Constraint function for the bias.
-    kernel: Weight matrix (TensorFlow variable or tensor).
-    bias: Bias vector, if applicable (TensorFlow variable or tensor).
-  """
+    def __init__(self, layers_to_combine,
+                 trainable=True,
+                 name=None,
+                 **kwargs):
+        super(CombinedDenseSameInput, self).__init__(trainable=trainable, name=name, **kwargs)
+        self.dense_layers = layers_to_combine
+        self.dense_kernels = [d.add_variable('kernel', [self.dense_layers[0].input_spec.axes[-1], d.units])
+                              for d in self.dense_layers]
 
-  def __init__(self, d1, d2,
-               trainable=True,
-               name=None,
-               **kwargs):
-    super(Dense, self).__init__(trainable=trainable, name=name
-                                **kwargs)
+    def build(self, input_shape):
+        input_shape = tensor_shape.TensorShape(input_shape)
+        if input_shape[-1].value is None:
+            raise ValueError('The last dimension of the inputs to `CombinedDenseSameInput` should be defined. '
+                             'Found `None`.')
+        self.input_spec = base.InputSpec(min_ndim=2,
+                                         axes={-1: input_shape[-1].value})
+        self.built = True
 
-  def build(self, input_shape):
-    input_shape = tensor_shape.TensorShape(input_shape)
-    if input_shape[-1].value is None:
-      raise ValueError('The last dimension of the inputs to `Dense` '
-                       'should be defined. Found `None`.')
-    self.input_spec = base.InputSpec(min_ndim=2,
-                                     axes={-1: input_shape[-1].value})
-    self.kernel = self.add_variable('kernel',
-                                    shape=[input_shape[-1].value, self.units],
-                                    initializer=self.d1.kernel_initializer,
-                                    regularizer=self.d1.kernel_regularizer,
-                                    constraint=self.d1.kernel_constraint,
-                                    dtype=self.d1.dtype,
-                                    trainable=True)
-    if self.use_bias:
-      self.bias = self.add_variable('bias',
-                                    shape=[self.d1.units + self.d2.units,],
-                                    initializer=self.bias_initializer,
-                                    regularizer=self.bias_regularizer,
-                                    constraint=self.bias_constraint,
-                                    dtype=self.dtype,
-                                    trainable=True)
-    else:
-      self.bias = None
-    self.built = True
-
-  def call(self, inputs):
-    input_shape = tensor_shape.TensorShape(inputs.shape)
-    if input_shape[-1].value is None:
-      raise ValueError('The last dimension of the inputs to `Dense` '
-                       'should be defined. Found `None`.')
-    self.input_spec = base.InputSpec(min_ndim=2,
-                                     axes={-1: input_shape[-1].value})
-    inputs = ops.convert_to_tensor(inputs, dtype=self.dtype)
-    shape = inputs.get_shape().as_list()
-    self.kernel = self.add_variable('kernel_combined', shape=[inputs_shape[-1], self.d1.units + self.d2.units])
-    if len(shape) > 2:
-      # Broadcasting is required for the inputs.
-      outputs = standard_ops.tensordot(inputs, self.kernel, [[len(shape) - 1],
+    def call(self, inputs, **kwargs):
+        combined_kernel = tf.concat(axis=1, values=self.dense_kernels, name='combined_kernel')
+        input_shape = tensor_shape.TensorShape(inputs.shape)
+        if input_shape[-1].value is None:
+            raise ValueError('The last dimension of the inputs to `Dense` '
+                             'should be defined. Found `None`.')
+        self.input_spec = base.InputSpec(min_ndim=2,
+                                         axes={-1: input_shape[-1].value})
+        inputs = ops.convert_to_tensor(inputs, dtype=self.dtype)
+        shape = inputs.get_shape().as_list()
+        if len(shape) > 2:
+            # Broadcasting is required for the inputs.
+            outputs = standard_ops.tensordot(inputs, combined_kernel, [[len(shape) - 1],
                                                              [0]])
-      # Reshape the output back to the original ndim of the input.
-      if context.in_graph_mode():
-        output_shape = shape[:-1] + [self.units]
-        outputs.set_shape(output_shape)
-    else:
-      outputs = standard_ops.matmul(inputs, self.kernel)
-    if self.use_bias:
-      outputs = nn.bias_add(outputs, (self.d1.bias * self.d1.units) + (self.d2.bias * self.d2.units)
-    if self.activation is not None:
-      return self.activation(outputs)  # pylint: disable=not-callable
-    return outputs
+            # # Reshape the output back to the original ndim of the input.
+            # if context.in_graph_mode():
+            #   output_shape = shape[:-1] + [self.units]
+            #   outputs.set_shape(output_shape)
+        else:
+            outputs = standard_ops.matmul(inputs, combined_kernel)
+        prev = None
+        output_ops = []
+        bias_combined = False
+        activations_combined = False
+        if all([d.use_bias for d in self.dense_layers]):
+            combined_bias = tf.concat(axis=0, values=[d.bias for d in self.dense_layers])
+            outputs = nn.bias_add(outputs, combined_bias)
+            bias_combined = True
+        activations = {d.activation for d in self.dense_layers}
+        if None not in activations and len(activations) == 1:
+            outputs = activations.pop()(outputs)
+            activations_combined = True
+        for d in self.dense_layers:
+            if prev is None:
+                layer_output = outputs[:, :d.units]
+            else:
+                layer_output = outputs[:, prev:(prev+d.units)]
+            prev = d.units
+            if d.use_bias and not bias_combined:
+                print(d.bias)
+                print(layer_output)
+                layer_output = nn.bias_add(layer_output, d.bias)
+            if d.activation is not None and not activations_combined:
+                layer_output = d.activation(layer_output)
+            output_ops.append(layer_output)
+        # this can be significantly more optimized - we could bias_add all at once
+        return output_ops
 
-  def compute_output_shape(self, input_shape):
-    input_shape = tensor_shape.TensorShape(input_shape)
-    input_shape = input_shape.with_rank_at_least(2)
-    if input_shape[-1].value is None:
-      raise ValueError(
-          'The innermost dimension of input_shape must be defined, but saw: %s'
-          % input_shape)
-    return input_shape[:-1].concatenate(self.units)
+    def compute_output_shape(self, input_shape):
+        input_shape = tensor_shape.TensorShape(input_shape)
+        input_shape = input_shape.with_rank_at_least(2)
+        if input_shape[-1].value is None:
+            raise ValueError(
+              'The innermost dimension of input_shape must be defined, but saw: %s'
+              % input_shape)
+        return input_shape[:-1].concatenate(self.units)
 
 
-def combinedDenseSameInput(
-    d1, d2,
-    trainable=True,
-    name=None,
-    reuse=None):
-  """Functional interface for the densely-connected layer.
+def combinedDenseSameInput(inputs, layers_to_combine, trainable=True, name=None, reuse=None):
+    """Functional interface for the combined densely-connected layer, using the same input.
 
-  This layer implements the operation:
-  `outputs = activation(inputs.kernel + bias)`
-  Where `activation` is the activation function passed as the `activation`
-  argument (if not `None`), `kernel` is a weights matrix created by the layer,
-  and `bias` is a bias vector created by the layer
-  (only if `use_bias` is `True`).
+    This layer implements the operation:
+    `outputs = activation(inputs.kernel + bias)`
+    Where `activation` is the activation function passed as the `activation`
+    argument (if not `None`), `kernel` is a weights matrix created by the layer,
+    and `bias` is a bias vector created by the layer
+    (only if `use_bias` is `True`).
 
-  Arguments:
+    Arguments:
     inputs: Tensor input.
-    units: Integer or Long, dimensionality of the output space.
-    activation: Activation function (callable). Set it to None to maintain a
-      linear activation.
-    use_bias: Boolean, whether the layer uses a bias.
-    kernel_initializer: Initializer function for the weight matrix.
-      If `None` (default), weights are initialized using the default
-      initializer used by `tf.get_variable`.
-    bias_initializer: Initializer function for the bias.
-    kernel_regularizer: Regularizer function for the weight matrix.
-    bias_regularizer: Regularizer function for the bias.
-    activity_regularizer: Regularizer function for the output.
-    kernel_constraint: An optional projection function to be applied to the
-        kernel after being updated by an `Optimizer` (e.g. used to implement
-        norm constraints or value constraints for layer weights). The function
-        must take as input the unprojected variable and must return the
-        projected variable (which must have the same shape). Constraints are
-        not safe to use when doing asynchronous distributed training.
-    bias_constraint: An optional projection function to be applied to the
-        bias after being updated by an `Optimizer`.
+    d1: Dense layer to merge
+    d2: Dense layer to merge
     trainable: Boolean, if `True` also add variables to the graph collection
       `GraphKeys.TRAINABLE_VARIABLES` (see `tf.Variable`).
     name: String, the name of the layer.
     reuse: Boolean, whether to reuse the weights of a previous layer
       by the same name.
 
-  Returns:
+    Returns:
     Output tensor the same shape as `inputs` except the last dimension is of
     size `units`.
 
-  Raises:
+    Raises:
     ValueError: if eager execution is enabled.
-  """
-  layer = Dense(units,
-                activation=activation,
-                use_bias=use_bias,
-                kernel_initializer=kernel_initializer,
-                bias_initializer=bias_initializer,
-                kernel_regularizer=kernel_regularizer,
-                bias_regularizer=bias_regularizer,
-                activity_regularizer=activity_regularizer,
-                kernel_constraint=kernel_constraint,
-                bias_constraint=bias_constraint,
-                trainable=trainable,
-                name=name,
-                dtype=inputs.dtype.base_dtype,
-                _scope=name,
-                _reuse=reuse)
-  return layer.apply(inputs)
+    """
+    layer = CombinedDenseSameInput(layers_to_combine,
+                                   trainable=trainable,
+                                   name=name,
+                                   dtype=inputs.dtype.base_dtype,
+                                   _scope=name,
+                                   _reuse=reuse)
+    return layer.apply(inputs)
