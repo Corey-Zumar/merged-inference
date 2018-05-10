a6a04b734f0c2192273b78a22a9e1b36c0bf9c32
Ankit Mathur <ankmathur96@gmail.com>
2018-05-10 06:38:58 +0000

    run experiment and get results

--- tensorflow_models/eval_dense_batches.sh
+++ tensorflow_models/eval_dense_batches.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-#for i in 2 4 8 16 32 64 128 256 512
-for i in 8 32 64 128 256 512
-do
+for i in 2 4 8 16 32 64 128 256 512
+do   
   python mnist_dense_ensemble.py --n_ensemble=8 --epochs=0.2 --serving_model_path=models/dense_experimental/ --combine_dense --n_trials=200 --batch_size=$i
 done
-#for i in 2 4 8 16 32 64 128 256 512
-#do
-#  python mnist_dense_ensemble.py --n_ensemble=8 --epochs=0.2 --serving_model_path=models/dense_experimental/ --n_trials=200 --batch_size=$i
-#done
+for i in 2 4 8 16 32 64 128 256 512
+do
+  python mnist_dense_ensemble.py --n_ensemble=8 --epochs=0.2 --serving_model_path=models/dense_experimental/ --n_trials=200 --batch_size=$i
+done
